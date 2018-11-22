2a5af2d1b0bb97e384ea0017dec3d49f605e159e
Olsi Seferi <oseferi@ikub.local>
2018-11-22 15:10:05 +0100

    NGRX SETUP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,20 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { CoreModule } from './core/core.module';
+import { AngularFireModule } from '@angular/fire';
+import {  AngularFireAuthModule } from '@angular/fire/auth';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAVcGFoi_F-kM1vGiCyPJ9XiZETTpJ4Gtg',
+  authDomain: 'chronos-47.firebaseapp.com',
+  databaseURL: 'https://chronos-47.firebaseio.com',
+  projectId: 'chronos-47',
+  storageBucket: 'chronos-47.appspot.com',
+  messagingSenderId: '883424200025',
+};
 
 @NgModule({
   declarations: [
@@ -15,7 +29,12 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    CoreModule
+    CoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
